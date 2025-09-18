940d17ae04c4ac68aef191056035cecd72a56cc5
Maxence Valiere <maxence.valiere@implicity.com>
2025-09-18 17:05:48 +0200

    chore: migrate ESLint configuration to new format and update linting scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +10,36 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
-
-export default eslintConfig;
+export default defineConfig([
+  globalIgnores([
+    "**/node_modules/**",
+    ".git/",
+    "**/.pnp/**",
+    "**/.yarn/**",
+    "**/coverage/**",
+    "**/.next/**",
+    "out/**",
+    "build/**",
+    ".DS_Store",
+    "*.pem",
+    ".vscode/**",
+    "npm-debug.log*",
+    "yarn-debug.log*",
+    "yarn-error.log*",
+    ".pnpm-debug.log*",
+    ".env*",
+    ".vercel/**",
+    "*.tsbuildinfo",
+    "next-env.d.ts",
+    "backup/**",
+    "*.old.bak",
+  ]),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
+  },
+]);
