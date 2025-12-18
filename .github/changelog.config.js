bbcffc6fa0b1f5adb50b885bb09744dd3684abdd
Maxence Valiere <maxence.valiere@implicity.com>
2025-12-18 16:21:27 +0100

    build: fix conventional-changelog-conventionalcommits require

--- .github/changelog.config.js
+++ .github/changelog.config.js
@@ -1,18 +1,23 @@
 "use strict";
-const config = require("conventional-changelog-conventionalcommits");
+const preset = require("conventional-changelog-conventionalcommits");
 
-module.exports = config({
-  types: [
-    { type: "feat", section: "Features" },
-    { type: "fix", section: "Bug Fixes" },
-    { type: "chore", section: "Chores" },
-    { type: "docs", hidden: true },
-    { type: "style", section: "Style" },
-    { type: "refactor", section: "Refactoring" },
-    { type: "perf", section: "Performance" },
-    { type: "test", hidden: true },
-    { type: "build", section: "Build System or External Dependencies" },
-    { type: "ci", hidden: true },
-    { type: "revert", section: "Reverts" },
-  ],
-});
+module.exports = async (config) => {
+  const factory = typeof preset === "function" ? preset : preset.default;
+  const options = await factory({
+    ...config,
+    types: [
+      { type: "feat", section: "Features" },
+      { type: "fix", section: "Bug Fixes" },
+      { type: "chore", section: "Chores" },
+      { type: "docs", hidden: true },
+      { type: "style", section: "Style" },
+      { type: "refactor", section: "Refactoring" },
+      { type: "perf", section: "Performance" },
+      { type: "test", hidden: true },
+      { type: "build", section: "Build System or External Dependencies" },
+      { type: "ci", hidden: true },
+      { type: "revert", section: "Reverts" },
+    ],
+  });
+  return options;
+};
